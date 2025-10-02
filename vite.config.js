4d96a0520b764ffdfceb4fb2c1b3083a3c28ed87
SehrishHussain <sehrishaslam1@hotmail.com>
2025-10-02 20:58:52 +0500

    deploying env

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,13 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-  // Load env vars from .env files + system
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
     plugins: [react()],
     define: {
-      // Make sure VITE_USE_MOCK is always defined (stringified because Vite replaces code at build time)
       'import.meta.env.VITE_USE_MOCK': JSON.stringify(env.VITE_USE_MOCK || 'false'),
     },
-    base: process.env.VITE_BASE_PATH || "/Blogify"
+    base: process.env.VITE_BASE_PATH || "/Blogify",
   }
 })

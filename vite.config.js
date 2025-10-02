57c6ce97e8f22d2e282ac9a27631482528acd24e
SehrishHussain <sehrishaslam1@hotmail.com>
2025-10-02 14:10:55 +0500

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,6 @@ export default defineConfig(({ mode }) => {
       // Make sure VITE_USE_MOCK is always defined (stringified because Vite replaces code at build time)
       'import.meta.env.VITE_USE_MOCK': JSON.stringify(env.VITE_USE_MOCK || 'false'),
     },
+    base: process.env.VITE_BASE_PATH || "/Blogify"
   }
 })
